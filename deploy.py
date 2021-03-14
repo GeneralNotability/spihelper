818cdb4d3ebdefa87dd27174c388c98109a4447f
General Notability <generalnotability@gmail.com>
2021-03-14 16:20:00 -0400

    rework the deploy script some more

--- deploy.py
+++ deploy.py
@@ -2,18 +2,18 @@ import argparse
 import pywikibot
 
 
-def main(target, file_to_deploy, revision_id):
+def main(target, file_to_deploy, summary):
     site = pywikibot.Site('en', 'wikipedia')
     page = pywikibot.Page(site, target)
     with open(file_to_deploy, 'r') as source_file:
         page.text = source_file.read()
 
-    page.save(f'Deploying revision {revision_id}')
+    page.save(summary)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--target', help='Target page')
     parser.add_argument('--file', help='File to deploy')
-    parser.add_argument('--revision-id', help='Revision ID')
+    parser.add_argument('--summary', help='Edit summary')
     args = parser.parse_args()
-    main(args.target, args.file, args.revision_id)
+    main(args.target, args.file, args.summary)
